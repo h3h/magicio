067ef7c1e8ec3b72e612598ad379bfb0a19fccac
Brad Fults <bfults@gmail.com>
2013-04-14 13:52:45 -0500

    Get parsing working with first example.

--- src/jquery.magicio.coffee
+++ src/jquery.magicio.coffee
@@ -5,31 +5,75 @@
 * Released under the MIT License
 * https://github.com/h3h/magicio/LICENSE
 *
-* Thanks to Dave Rupert for Lettering.js
+* Thanks to Dave Rupert for inspiration from Lettering.js
 * https://github.com/davatron5000/Lettering.js
 ###
 
 (($) ->
   settings = {}
+  UNIQUE_STR = "eefec303079ad17405c889e092e105b0"
 
-  log = (msg) ->
-    console?.log msg if settings.debug
+  log = (msg, others...) ->
+    if settings.debug
+      if others.length then console?.log(msg, others) else console?.log(msg)
+
+  class Action
+    constructor: (@nextElement, @prevElement, @breakClasses, @breakType, @timing) ->
 
   methods =
     init: (options) ->
       settings =
-        requireActionOnBreak: false
         debug: false
+        defaultActionOnPause: 'time'
 
       settings = $.extend settings, options
 
       @each () ->
-        log "Initializing magicio for element with id \"#{@id}\""
-        $(@).lettering('words').data('magicio', {thing: 'bob'})
+        log "Initializing Magicio for element with id \"#{@id}\""
+        jqEl = $(@)
+        methods.parse(jqEl)
+        #methods.buildActions(jqEl)
+
+    destroy: (jqEl) ->
+      @each () ->
+        log "Destroying Magicio for element with id \"#{@id}\""
+        $(@).removeData('magicio')
+
+    buildActions: (jqEl) ->
+      jqEl.data('magicio', {actions: actions})
+
+    parse: (jqEl) ->
+      jqEl.trigger('beforeparse')
+
+      # First parse & tag explicit breaks
+      breaks = jqEl.children('.m-break')
+      log "Breaks: #{breaks.length}"
+      if breaks.length is 0
+        breaks = jqEl
+      else
+        breaks.addClass('m-break-parsed')
+
+      # Then parse, wrap & tag explicit pauses
+      breaks.each () ->
+        elBreak = $(@)
+        pauses = elBreak.children('br.m-pause')
+        log "Pauses: #{pauses.length}"
+        pauses.replaceWith(UNIQUE_STR)
+        fragments = elBreak.html().split(UNIQUE_STR)
+        if fragments.length > 0
+          fragments = $.map fragments, (frag, i) ->
+            classes = if pauses[i-1] then pauses[i-1].className.replace(/\bm-pause\b/, '') else ''
+            "<span class='m-pause-parsed #{classes}'>#{frag}</span>"
+          elBreak.html(fragments.join(''))
+
+      # Collect all actors & store them for the play, then trigger an event
+      actors = jqEl.find('.m-break-parsed, span.m-pause-parsed')
+      jqEl.data('magicio', {actors: actors})
+      jqEl.trigger($.Event('afterparse', {actors: actors}))
 
     run: () ->
       @each () ->
-        log $(@).data('magicio')
+        log "Magicio found on element: %o", $(@).data('magicio')
 
   $.fn.extend
     magicio: (method) ->
--- src/jquery.magicio.js
+++ src/jquery.magicio.js
@@ -6,38 +6,109 @@
 * Released under the MIT License
 * https://github.com/h3h/magicio/LICENSE
 *
-* Thanks to Dave Rupert for Lettering.js
+* Thanks to Dave Rupert for inspiration from Lettering.js
 * https://github.com/davatron5000/Lettering.js
 */
 
 
 (function() {
+  var __slice = [].slice;
+
   (function($) {
-    var log, methods, settings;
+    var Action, UNIQUE_STR, log, methods, settings;
 
     settings = {};
-    log = function(msg) {
+    UNIQUE_STR = "eefec303079ad17405c889e092e105b0";
+    log = function() {
+      var msg, others;
+
+      msg = arguments[0], others = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
       if (settings.debug) {
-        return typeof console !== "undefined" && console !== null ? console.log(msg) : void 0;
+        if (others.length) {
+          return typeof console !== "undefined" && console !== null ? console.log(msg, others) : void 0;
+        } else {
+          return typeof console !== "undefined" && console !== null ? console.log(msg) : void 0;
+        }
       }
     };
+    Action = (function() {
+      function Action(nextElement, prevElement, breakClasses, breakType, timing) {
+        this.nextElement = nextElement;
+        this.prevElement = prevElement;
+        this.breakClasses = breakClasses;
+        this.breakType = breakType;
+        this.timing = timing;
+      }
+
+      return Action;
+
+    })();
     methods = {
       init: function(options) {
         settings = {
-          requireActionOnBreak: false,
-          debug: false
+          debug: false,
+          defaultActionOnPause: 'time'
         };
         settings = $.extend(settings, options);
         return this.each(function() {
-          log("Initializing magicio for element with id \"" + this.id + "\"");
-          return $(this).lettering('words').data('magicio', {
-            thing: 'bob'
-          });
+          var jqEl;
+
+          log("Initializing Magicio for element with id \"" + this.id + "\"");
+          jqEl = $(this);
+          return methods.parse(jqEl);
         });
+      },
+      destroy: function(jqEl) {
+        return this.each(function() {
+          log("Destroying Magicio for element with id \"" + this.id + "\"");
+          return $(this).removeData('magicio');
+        });
+      },
+      buildActions: function(jqEl) {
+        return jqEl.data('magicio', {
+          actions: actions
+        });
+      },
+      parse: function(jqEl) {
+        var actors, breaks;
+
+        jqEl.trigger('beforeparse');
+        breaks = jqEl.children('.m-break');
+        log("Breaks: " + breaks.length);
+        if (breaks.length === 0) {
+          breaks = jqEl;
+        } else {
+          breaks.addClass('m-break-parsed');
+        }
+        breaks.each(function() {
+          var elBreak, fragments, pauses;
+
+          elBreak = $(this);
+          pauses = elBreak.children('br.m-pause');
+          log("Pauses: " + pauses.length);
+          pauses.replaceWith(UNIQUE_STR);
+          fragments = elBreak.html().split(UNIQUE_STR);
+          if (fragments.length > 0) {
+            fragments = $.map(fragments, function(frag, i) {
+              var classes;
+
+              classes = pauses[i - 1] ? pauses[i - 1].className.replace(/\bm-pause\b/, '') : '';
+              return "<span class='m-pause-parsed " + classes + "'>" + frag + "</span>";
+            });
+            return elBreak.html(fragments.join(''));
+          }
+        });
+        actors = jqEl.find('.m-break-parsed, span.m-pause-parsed');
+        jqEl.data('magicio', {
+          actors: actors
+        });
+        return jqEl.trigger($.Event('afterparse', {
+          actors: actors
+        }));
       },
       run: function() {
         return this.each(function() {
-          return log($(this).data('magicio'));
+          return log("Magicio found on element: %o", $(this).data('magicio'));
         });
       }
     };
